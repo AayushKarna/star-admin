c244ce10c2115d20c91cc6920d926abbf3052f34
Aayush Karna <aayushkarna6191@gmail.com>
2025-03-30 14:22:42 +0545

    pre production

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -32,7 +32,7 @@ export default function DashboardLayout({
         <AppSidebar />
         <SidebarInset>
           <Header />
-          <div className="flex flex-1 flex-col gap-4 p-4 bg-gray-100 px-6">
+          <div className="flex flex-1 flex-col gap-4 py-4 bg-gray-100 px-6">
             {children}
           </div>
         </SidebarInset>
