cc159ed63d5e2e4422005a03867239a4361dd789
Aayush Karna <aayushkarna6191@gmail.com>
2025-04-12 10:13:13 +0545

    changed constant for live

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://api.starhifi.com';
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://api.starhifi.com';
+// const BASE_URL = 'http://localhost:8080';
 const TIMEOUT_SEC = 15;
 
 export { BASE_URL, TIMEOUT_SEC };
